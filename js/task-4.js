2e91f86baa74d51800a55f12bb5320bdc1b0c94b
JuliaCicer13 <137887271+JuliaCicer13@users.noreply.github.com>
2024-02-22 01:27:55 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -28,3 +28,40 @@ loginForm.addEventListener('submit', function (event) {
   // Reset form fields
   this.reset();
 });
+
+const registerForm = document.querySelector(".form");
+
+registerForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const login = form.elements.login.value;
+  const password = form.elements.password.value;
+  
+  if (login === "" || password === "") {
+    return console.log("Please fill in all the fields!");
+  }
+
+  console.log(`Login: ${login.value}, Password: ${password.value}`);
+  form.reset();
+}
+const textInput = document.querySelector(".text-input");
+const setFocusBtn = document.querySelector('[data-action="set"]');
+const removeFocusBtn = document.querySelector('[data-action="remove"]');
+
+setFocusBtn.addEventListener("click", () => {
+  textInput.focus();
+});
+
+removeFocusBtn.addEventListener("click", () => {
+  textInput.blur();
+});
+
+textInput.addEventListener("focus", () => {
+  textInput.value = "This input has focus";
+});
+
+textInput.addEventListener("blur", () => {
+  textInput.value = "";
+});
