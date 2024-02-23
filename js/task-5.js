9483a72e4146dd0bf6f45083c638d7e513f7c710
JuliaCicer13 <137887271+JuliaCicer13@users.noreply.github.com>
2024-02-24 00:25:48 +0200

    Update task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -1,11 +1,13 @@
 const changeColorButton = document.querySelector('.change-color');
-const colorSpan = document.querySelector('.color');
+const changeColorSpan = document.querySelector('color');
+
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, '0')}`;
+    .padStart(6, 0)}`;
 }
+
 
 changeColorButton.addEventListener('click', function () {
   const randomColor = getRandomHexColor();
