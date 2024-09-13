2d7a7a6d8bab72f6af4af40b6dd3ee40c2bb43e4
JuliaCicer13 <137887271+JuliaCicer13@users.noreply.github.com>
2024-09-14 01:59:30 +0300

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,13 +1,14 @@
 
-const categoryItems = document.querySelectorAll('li.item');
-console.log(`Total categories: ${categoryItems.length}`);
+const categoryItem = document.querySelectorAll('li.item');
+console.log(`Number of categories: ${categoryItem.length}`)
 
-categoryItems.forEach((category) => {
-  const categoryName = category.querySelector('h2').textContent;
-  const categoryElements = category.querySelectorAll('li').length;
 
-  console.log(`Category: ${categoryName}`);
-  console.log(`Elements in category: ${categoryElements}`);
+categoryItem.forEach((category) => {
+  const textItem = category.querySelector('h2').textContent;
+  const allItem = category.querySelectorAll('li').length;
+  console.log(`Category: ${textItem}`);
+  console.log(`Elements: ${allItem}`);
+
 });
 
 
